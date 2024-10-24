ea0f988f593e930a956a83f48b70173b40628079
Camilla Harris <camilla.harris@ucl.ac.uk>
2024-10-24 12:34:24 +0100

    Variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,21 +1,26 @@
 variable "img_display_name" {
-  type = string
+  type        = string
+  description = "Display name of an image in the harvester-public namespace"
 }
 
 variable "namespace" {
-  type = string
+  type        = string
+  description = "Namespace that the SSH public key and network are already deployed in, and that the VM will be deployed in"
 }
 
 variable "network_name" {
-  type = string
+  type        = string
+  description = "Name of a network in the specified namespace"
 }
 
 variable "prefix" {
-  type = string
+  type        = string
+  description = "Prefix for the VM name"
 }
 
 variable "public_key" {
-  type = string
+  type        = string
+  description = "Name of an SSH key in the specified namespace"
 }
 
 variable "vm_count" {
